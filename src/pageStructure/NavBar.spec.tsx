c5e5f540046505e988f8b55932fb920847c7d2bd
Jerzy Lozenko <112312492+IuriiLozenko@users.noreply.github.com>
2023-03-09 21:07:38 +0100

    Navbar test

--- src/pageStructure/NavBar.spec.tsx
+++ src/pageStructure/NavBar.spec.tsx
@@ -1,25 +1,19 @@
-import React from 'react'
 import { render, screen } from '@testing-library/react'
 import NavBar from './NavBar'
 import { BrowserRouter } from 'react-router-dom'
 import userEvent from '@testing-library/user-event'
 
-test('click to the button redirect', () => {
+test('click to the button', () => {
     render(
         <BrowserRouter>
             <NavBar />
         </BrowserRouter>,
     )
-    const linkHome = screen.getByText(/home/i)
-    expect(linkHome).toBeInTheDocument()
+    const homeLink = screen.getByText(/home/i)
+    expect(homeLink).toBeInTheDocument()
 
     const employeeList = screen.getByText(/employee list/i)
 
-    console.log(window.location.pathname)
-
     userEvent.click(employeeList)
-
-    console.log(window.location.pathname)
-
     expect(window.location.pathname).toBe('/employees-list')
 })
