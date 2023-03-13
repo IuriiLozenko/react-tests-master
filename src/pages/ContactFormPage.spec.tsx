1b91860a3fcdaec016751dae3386f3145caa91c9
Jerzy Lozenko <112312492+IuriiLozenko@users.noreply.github.com>
2023-03-13 18:51:47 +0100

    Contact form page test

--- src/pages/ContactFormPage.spec.tsx
+++ src/pages/ContactFormPage.spec.tsx
@@ -1,22 +1,18 @@
 import React from 'react'
 import { render, screen, waitFor } from '@testing-library/react'
 import ContactFormPage from './ContactFormPage'
-import { BrowserRouter } from 'react-router-dom'
 import userEvent from '@testing-library/user-event'
 
 test('renders learn react link', async () => {
     window.alert = jest.fn()
 
     render(
-        <BrowserRouter>
             <ContactFormPage />
-        </BrowserRouter>,
     )
-    expect(screen.getByText(/form with two states/i)).toBeInTheDocument()
-    const nameInput = screen.getByText(/first name/i)
-    userEvent.type(nameInput, 'Jan')
+    const firstNameInput = screen.getByText(/first name/i)
+    userEvent.type(firstNameInput, 'Paweł')
 
-    const surnameInput = screen.getByText(/first name/i)
+    const surnameInput = screen.getByText(/surname/i)
     userEvent.type(surnameInput, 'Kowalski')
 
     const btnButton = screen.getByRole('button', {
